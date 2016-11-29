e7fc8299795c3c99ef4b2a85338f91bd5fdc49a2
LiliMeng <lilimeng1103@gmail.com>
2016-11-29 10:45:36 -0800

    Update test_RFR.cpp

--- test_RFR.cpp
+++ test_RFR.cpp
@@ -2,7 +2,7 @@
 //  test_RFR.cpp
 //  RandomFeatureRegistration
 //
-//  Created by jimmy on 11/27/16.
+//  Created by Lili on 11/27/16.
 //  Copyright (c) 2016 UBC. All rights reserved.
 //
 
@@ -13,13 +13,15 @@ void test_RandomFeatureMatching()
 {
     
     char rgb_img_file1[] = "/Users/jimmy/Desktop/images/7_scenes/chess/seq-01/frame-000000.color.png";
-    char rgb_img_file2[] = "/Users/jimmy/Desktop/images/7_scenes/chess/seq-01/frame-000001.color.png";
+    char rgb_img_file2[] = "/Users/jimmy/Desktop/images/7_scenes/chess/seq-01/frame-000100.color.png";
     
     char depth_img_file1[] = "/Users/jimmy/Desktop/images/7_scenes/chess/seq-01/frame-000000.depth.png";
-    char depth_img_file2[] = "/Users/jimmy/Desktop/images/7_scenes/chess/seq-01/frame-000001.depth.png";
+    char depth_img_file2[] = "/Users/jimmy/Desktop/images/7_scenes/chess/seq-01/frame-000100.depth.png";
     
     
-    const char* dataset_param_filename="/Users/jimmy/Desktop/PR2017/4scenes_param.txt";
+    const char* dataset_param_filename="/Users/jimmy/Desktop/PR2017/7scenes_param.txt";
+    
+    const char* tree_param_filename="/Users/jimmy/Desktop/PR2017/RF_param.txt";
     
     cv::Mat rgb_img1, rgb_img2;
    
@@ -34,6 +36,8 @@ void test_RandomFeatureMatching()
     
     DatasetParameter dataset_param;
     
+    std::cout<<"hello1, no problem in dataset_param "<<std::endl;
+    
     dataset_param.readFromFileDataParameter(dataset_param_filename);
     double depth_factor = dataset_param.depth_factor_;
     
@@ -46,11 +50,24 @@ void test_RandomFeatureMatching()
     double min_depth = dataset_param.min_depth_;
     double max_depth = dataset_param.max_depth_;
     
-    int num_sample_source = 5000;
-    int num_sample_target = 5000;
+    
+    RFRTreeParameter tree_param;
+    
+    tree_param.readFromFile(tree_param_filename);
+    
+    
+    
+    int num_sample_source = tree_param.sampler_num_per_frame_;
+    int max_frame_num = tree_param.max_frame_num_;
+    int tree_num = tree_param.tree_num_;
+    int max_tree_depth = tree_param.max_depth_;
+    
+    int num_sample_target = 50;
     int image_index = 0;
 
     bool use_depth = 0;
+    
+    std::cout<<"hello2, no problem in use_depth = 0"<<std::endl;
     
     vector<RFRSourceSample> source_samples = RFRUtil::randomSampleFromRgbdImages(rgb_img_file1,
                                                                                  depth_img_file1,
@@ -64,10 +81,6 @@ void test_RandomFeatureMatching()
     
     
     RFRTree tree;
-    RFRTreeParameter param;
-    param.verbose_ = true;
-    param.min_leaf_node_ = 1;
-    param.max_depth_ = 15;
     
     vector<unsigned int> indices;
     
@@ -76,16 +89,20 @@ void test_RandomFeatureMatching()
         indices.push_back(i);
     }
     
+    std::cout<<"hello3, no problem in indices.push_bakc(i)"<<std::endl;
+    
     vector<cv::Mat> rgbImages;
     
     rgbImages.push_back(rgb_img1);
     
-    tree.buildTree(source_samples, indices, rgbImages, param);
+    tree.buildTree(source_samples, indices, rgbImages, tree_param);
 
     vector<RFRTargetSample> predictions;
     vector<cv::Point2d> distance;
     
     vector<cv::Point2d> source_points, target_points;
+    
+    std::cout<<"hello4, no problem in buildTree"<<std::endl;
     
     for(int i=0; i<num_sample_target; i++)
     {
@@ -100,16 +117,25 @@ void test_RandomFeatureMatching()
         }
     }
     
+    assert(source_points.size()==target_points.size());
+    
+    std::cout<<"hello5, no problem in predictions.push_back(pred)"<<std::endl;
+    
     cv::Mat matches;
     
     const int sample_num_match = 50;
     
-    CvDraw::draw_match_vertical(rgb_img1,
-                                rgb_img2,
-                                source_points,
-                                target_points,
-                                matches,
-                                sample_num_match);
-  
+   
+    hconcat(rgb_img1,rgb_img2,rgb_img1);
     
+    for(int i=0; i<target_points.size(); i++)
+    {
+        cv::line(rgb_img1, source_points[i], target_points[i]+cv::Point2d(rgb_img2.cols, 0), cv::Scalar(0, 255, 0), 1, 8, 0);
+    }
+  
+    cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE );// Create a window for display.
+    cv::imshow( "Display window", rgb_img1 );                   // Show our image inside it.
+    
+    cv::waitKey(0);                                          // Wait for a keystroke in the window
+
 }
