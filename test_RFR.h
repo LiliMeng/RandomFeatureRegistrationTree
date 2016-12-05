fd28c7556fd89ae5155ed04bce0cb024822a109d
LiliMeng <lilimeng1103@gmail.com>
2016-12-04 20:19:26 -0800

    Update test_RFR.h

--- test_RFR.h
+++ test_RFR.h
@@ -2,7 +2,7 @@
 //  test_RFR.h
 //  RandomFeatureRegistration
 //
-//  Created by jimmy on 11/27/16.
+//  Created by Lili on 11/27/16.
 //  Copyright (c) 2016 UBC. All rights reserved.
 //
 
@@ -14,9 +14,25 @@
 #include "RFR_util.h"
 #include "RFRTree.h"
 #include "cvxIO.hpp"
-#include "cv_draw.hpp"
+#include "KLT_Util.h"
+#include "ms7ScenesUtil.hpp"
+#include "cvxPoseEstimation.hpp"
+#include "Kabsch.h"
+#include "fstream"
+
+
 
 void test_RandomFeatureMatching();
 
+void test_KLT_tracking();
+
+void test_FAST_KLT_VO();
+
+void test_GoodFeatures_KLT_VO();
+
+void test_RandomFeature_KLT();
+
 
 #endif /* defined(__RandomFeatureRegistration__test_RFR__) */
+
+
